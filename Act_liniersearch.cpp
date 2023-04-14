15690cb94324d179879a20ca1edcec7dc87165e6
iffa28 <127277497+iffa28@users.noreply.github.com>
2023-04-14 18:21:58 +0700

    menambahkan array dengan panjang 20

--- Act_liniersearch.cpp
+++ Act_liniersearch.cpp
@@ -1,20 +1,72 @@
-// Act_liniersearch.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+int arr[20];  // Array to be searched
+int n;   // Number of elements in the array
+int i;   // Index pf Array element
+
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 element.\n\n";
+	}
+
+	// Accept array elements
+	cout << "\n--------------------\n";
+	cout << " Enter array elements \n";
+	cout << "----------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
+}
+
+
+
+
+
+void LinierSearch()
+{
+	char ch;
+	int comparisons; // Number of comprisons
+
+	do
+	{
+		// Accept the number to be searched
+		cout << "\nEnter the element you want to search: "; //Langkah 1
+		int item;
+		cin >> item;
+
+		comparisons = 0;
+		for (i = 0; i < n; i++)  // Langkah 2, 3 dan 4
+		{
+			comparisons++;
+			if (arr[i] == item) //langkah 5A found
+			{
+				cout << "\n" << item << " found at position " << (i + 1) << endl;
+				break;
+			}
+		}
+
+		if (i == n) // langkah 58 not found
+			cout << "\n" << item << "not found in the array\n";
+		cout << "\nNumber of comparisons: " << comparisons << endl;
+
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
+	} while ((ch == 'y') || (ch == 'Y'));
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	input();
+	LinierSearch();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
